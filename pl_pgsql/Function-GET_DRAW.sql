4eab1cded4f512916e1b653854da1e5fbb97ac51
Guillaume TRUC <guillaume.truc@gmail.com>
2015-03-28 19:21:40 +0100

    Create Function-GET_DRAW.sql

---
+++ pl_pgsql/Function-GET_DRAW.sql
@@ -0,0 +1,121 @@
+-- Function: "GET_DRAW"(integer, character varying)
+
+-- DROP FUNCTION "GET_DRAW"(integer, character varying);
+
+CREATE OR REPLACE FUNCTION "GET_DRAW"(_id_result integer, _lang character varying)
+  RETURNS refcursor AS
+$BODY$
+declare
+    _c refcursor;
+    _id_sport integer;
+    _id_championship integer;
+    _id_event integer;
+    _id_subevent integer;
+    _id_subevent2 integer;
+    _type integer;
+    _columns text;
+    _joins text;
+    _level varchar(3)[];
+    _entity varchar(3)[];
+    _entity_id varchar(15)[];
+begin
+	SELECT
+		RS.id_sport, RS.id_championship, RS.id_event, RS.id_subevent, RS.id_subevent2
+	INTO
+		_id_sport, _id_championship, _id_event, _id_subevent, _id_subevent2
+	FROM
+		"RESULT" RS
+	WHERE
+		RS.id = _id_result;
+	-- Get entity type (person, country, team)
+	IF _id_subevent2 <> 0 THEN
+	    SELECT
+	        TP.number
+	    INTO
+	        _type
+	    FROM
+	        "EVENT" EV
+	        LEFT JOIN "TYPE" TP ON EV.id_type = TP.id
+	    WHERE
+	        EV.id = _id_subevent2;
+	ELSIF _id_subevent <> 0 THEN
+	    SELECT
+	        TP.number
+	    INTO
+	        _type
+	    FROM
+	        "EVENT" EV
+	        LEFT JOIN "TYPE" TP ON EV.id_type = TP.id
+	    WHERE
+	        EV.id = _id_subevent;
+	ELSIF _id_event <> 0 THEN
+	    SELECT
+	        TP.number
+	    INTO
+	        _type
+	    FROM
+	        "EVENT" EV
+	        LEFT JOIN "TYPE" TP ON EV.id_type = TP.id
+	    WHERE
+	        EV.id = _id_event;	        
+	ELSE
+	    SELECT DISTINCT
+	        TP.number
+	    INTO
+	        _type
+	    FROM
+	        "RESULT" RS
+	        LEFT JOIN "EVENT" EV ON RS.id_event = EV.id
+	        LEFT JOIN "TYPE" TP ON EV.id_type = TP.id
+	    WHERE
+	         RS.id_sport = _id_sport AND RS.id_championship = _id_championship;
+	END IF;
+
+	INSERT INTO "~REQUEST" VALUES (NEXTVAL('"~SQ_REQUEST"'), 'DR', _id_sport || '-' || _id_championship || '-' || _id_event, current_date);
+
+	-- Build entity-specific columns/joins
+	_level = '{qf1,qf1,qf2,qf2,qf3,qf3,qf4,qf4,sf1,sf1,sf2,sf2,f,f,thd,thd}';
+	_entity = '{en1,en2,en1,en2,en1,en2,en1,en2,en1,en2,en1,en2,en1,en2,en1,en2}';
+	_entity_id = '{DR.id1_qf1,DR.id2_qf1,DR.id1_qf2,DR.id2_qf2,DR.id1_qf3,DR.id2_qf3,DR.id1_qf4,DR.id2_qf4,DR.id1_sf1,DR.id2_sf1,DR.id1_sf2,DR.id2_sf2,RS.id_rank1,RS.id_rank2,DR.id1_thd,DR.id2_thd}';
+	_columns := '';
+	_joins := '';
+	FOR i IN 1..16 LOOP
+		IF _type < 10 THEN -- Person
+			_columns := _columns || ', PR' || i || '.id AS ' || _entity[i] || '_' || _level[i] || '_id, PR' || i || '.last_name AS ' || _entity[i] || '_' || _level[i] || '_str1, PR' || i || '.first_name AS ' || _entity[i] || '_' || _level[i] || '_str2';
+			_columns := _columns || ', PRTM' || i || '.id AS ' || _entity[i] || '_' || _level[i] || '_rel1_id, NULL AS ' || _entity[i] || '_' || _level[i] || '_rel1_code, PRTM' || i || '.label AS ' || _entity[i] || '_' || _level[i] || '_rel1_label';
+			_columns := _columns || ', PRCN' || i || '.id AS ' || _entity[i] || '_' || _level[i] || '_rel2_id, PRCN' || i || '.code AS ' || _entity[i] || '_' || _level[i] || '_rel2_code, PRCN' || i || '.label' || _lang || ' AS ' || _entity[i] || '_' || _level[i] || '_rel2_label';
+			_joins := _joins || ' LEFT JOIN "PERSON" PR' || i || ' ON ' || _entity_id[i] || ' = PR' || i || '.id';
+			_joins := _joins || ' LEFT JOIN "TEAM" PRTM' || i || ' ON PR' || i || '.id_team = PRTM' || i || '.id';
+			_joins := _joins || ' LEFT JOIN "COUNTRY" PRCN' || i || ' ON PR' || i || '.id_country = PRCN' || i || '.id';
+		ELSIF _type = 50 THEN -- Team
+			_columns := _columns || ', TM' || i || '.id AS ' || _entity[i] || '_' || _level[i] || '_id, NULL AS ' || _entity[i] || '_' || _level[i] || '_str1, TM' || i || '.label AS ' || _entity[i] || '_' || _level[i] || '_str2';
+			_columns := _columns || ', NULL AS ' || _entity[i] || '_' || _level[i] || '_rel1_id, NULL AS ' || _entity[i] || '_' || _level[i] || '_rel1_code, NULL AS ' || _entity[i] || '_' || _level[i] || '_rel1_label';
+			_columns := _columns || ', TMCN' || i || '.id AS ' || _entity[i] || '_' || _level[i] || '_rel2_id, TMCN' || i || '.code AS ' || _entity[i] || '_' || _level[i] || '_rel2_code, TMCN' || i || '.label' || _lang || ' AS ' || _entity[i] || '_' || _level[i] || '_rel2_label';
+			_joins := _joins || ' LEFT JOIN "TEAM" TM' || i || ' ON ' || _entity_id[i] || ' = TM' || i || '.id';
+			_joins := _joins || ' LEFT JOIN "COUNTRY" TMCN' || i || ' ON TM' || i || '.id_country = TMCN' || i || '.id';
+		ELSIF _type = 99 THEN -- Country
+			_columns := _columns || ', CN' || i || '.id AS ' || _entity[i] || '_' || _level[i] || '_id, CN' || i || '.code AS ' || _entity[i] || '_' || _level[i] || '_str1, CN' || i || '.label' || _lang || ' AS ' || _entity[i] || '_' || _level[i] || '_str2';
+			_columns := _columns || ', NULL AS ' || _entity[i] || '_' || _level[i] || '_rel1_id, NULL AS ' || _entity[i] || '_' || _level[i] || '_rel1_code, NULL AS ' || _entity[i] || '_' || _level[i] || '_rel1_label';
+			_columns := _columns || ', NULL AS ' || _entity[i] || '_' || _level[i] || '_rel2_id, NULL AS ' || _entity[i] || '_' || _level[i] || '_rel2_code, NULL AS ' || _entity[i] || '_' || _level[i] || '_rel2_label';
+			_joins := _joins || ' LEFT JOIN "COUNTRY" CN' || i || ' ON ' || _entity_id[i] || ' = CN' || i || '.id';
+		END IF;
+	END LOOP;
+
+	-- Open cursor
+	OPEN _c FOR EXECUTE
+	'SELECT
+		DR.id AS dr_id, ' || _type || ' AS dr_type, YR.label AS yr_label, DR.result_qf1 AS dr_result_qf1, DR.result_qf2 AS dr_result_qf2, DR.result_qf3 AS dr_result_qf3, DR.result_qf4 AS dr_result_qf4,
+		DR.result_sf1 AS dr_result_sf1, DR.result_sf2 AS dr_result_sf2, RS.result1 AS rs_result_f, DR.result_thd AS dr_result_thd' ||
+		_columns || '
+	FROM
+		"DRAW" DR
+		LEFT JOIN "RESULT" RS ON DR.id_result = RS.id
+		LEFT JOIN "YEAR" YR ON RS.id_year = YR.id' ||
+		_joins || '
+	WHERE
+		DR.id_result = ' || _id_result;
+	RETURN  _c;
+end;
+$BODY$
+  LANGUAGE plpgsql VOLATILE
+  COST 100;
