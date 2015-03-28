c534de2a6f3c0bd7862ce894441ff00cfee5cedb
Guillaume TRUC <guillaume.truc@gmail.com>
2015-03-28 19:27:21 +0100

    Create Table-CHAMPIONSHIP.sql

---
+++ pl_pgsql/Table-CHAMPIONSHIP.sql
@@ -0,0 +1,25 @@
+-- Table: "CHAMPIONSHIP"
+
+-- DROP TABLE "CHAMPIONSHIP";
+
+CREATE TABLE "CHAMPIONSHIP"
+(
+  id integer NOT NULL,
+  label character varying(40) NOT NULL,
+  label_fr character varying(40) NOT NULL,
+  website character varying(50) DEFAULT NULL::character varying,
+  id_member integer NOT NULL,
+  last_update timestamp without time zone NOT NULL DEFAULT now(),
+  "index" integer,
+  first_update timestamp without time zone NOT NULL DEFAULT now(),
+  url_wiki character varying(200),
+  ref smallint,
+  CONSTRAINT "CHAMPIONSHIP_pkey" PRIMARY KEY (id),
+  CONSTRAINT "CHAMPIONSHIP_id_member_fkey" FOREIGN KEY (id_member)
+      REFERENCES "~MEMBER" (id) MATCH SIMPLE
+      ON UPDATE NO ACTION ON DELETE NO ACTION,
+  CONSTRAINT "CHAMPIONSHIP_label_key" UNIQUE (label)
+)
+WITH (
+  OIDS=FALSE
+);
