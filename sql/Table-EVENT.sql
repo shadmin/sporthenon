e714507532308c291aac0175e91edceca746e911
Guillaume TRUC <guillaume.truc@gmail.com>
2015-03-30 20:42:24 +0200

    Create Table-EVENT.sql

---
+++ sql/Table-EVENT.sql
@@ -0,0 +1,28 @@
+-- Table: "EVENT"
+
+-- DROP TABLE "EVENT";
+
+CREATE TABLE "EVENT"
+(
+  id integer NOT NULL,
+  label character varying(45) NOT NULL,
+  label_fr character varying(45) NOT NULL,
+  id_type integer NOT NULL,
+  id_member integer NOT NULL,
+  last_update timestamp without time zone NOT NULL DEFAULT now(),
+  "index" integer,
+  website character varying(50),
+  first_update timestamp without time zone NOT NULL DEFAULT now(),
+  url_wiki character varying(200),
+  ref smallint,
+  CONSTRAINT "EVENT_pkey" PRIMARY KEY (id),
+  CONSTRAINT "EVENT_id_member_fkey" FOREIGN KEY (id_member)
+      REFERENCES "~MEMBER" (id) MATCH SIMPLE
+      ON UPDATE NO ACTION ON DELETE SET NULL,
+  CONSTRAINT "EVENT_id_type_fkey" FOREIGN KEY (id_type)
+      REFERENCES "TYPE" (id) MATCH SIMPLE
+      ON UPDATE NO ACTION ON DELETE SET NULL
+)
+WITH (
+  OIDS=FALSE
+);
