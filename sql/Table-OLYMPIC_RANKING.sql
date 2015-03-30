c3f626c97c14645e3f7f3e741582cfdbe75430ce
Guillaume TRUC <guillaume.truc@gmail.com>
2015-03-30 21:01:19 +0200

    Create Table-OLYMPIC_RANKING.sql

---
+++ sql/Table-OLYMPIC_RANKING.sql
@@ -0,0 +1,39 @@
+-- Table: "OLYMPIC_RANKING"
+
+-- DROP TABLE "OLYMPIC_RANKING";
+
+CREATE TABLE "OLYMPIC_RANKING"
+(
+  id integer NOT NULL,
+  id_olympics integer NOT NULL,
+  id_country integer NOT NULL,
+  count_gold smallint NOT NULL,
+  count_silver smallint NOT NULL,
+  count_bronze smallint NOT NULL,
+  id_member integer NOT NULL,
+  last_update timestamp without time zone NOT NULL DEFAULT now(),
+  first_update timestamp without time zone NOT NULL DEFAULT now(),
+  CONSTRAINT "OLYMPIC_RANKING_pkey" PRIMARY KEY (id),
+  CONSTRAINT "OLYMPIC_RANKING_id_country_fkey" FOREIGN KEY (id_country)
+      REFERENCES "COUNTRY" (id) MATCH SIMPLE
+      ON UPDATE NO ACTION ON DELETE SET NULL,
+  CONSTRAINT "OLYMPIC_RANKING_id_member_fkey" FOREIGN KEY (id_member)
+      REFERENCES "~MEMBER" (id) MATCH SIMPLE
+      ON UPDATE NO ACTION ON DELETE SET NULL,
+  CONSTRAINT "OLYMPIC_RANKING_id_olympics_fkey" FOREIGN KEY (id_olympics)
+      REFERENCES "OLYMPICS" (id) MATCH SIMPLE
+      ON UPDATE NO ACTION ON DELETE SET NULL
+)
+WITH (
+  OIDS=FALSE
+);
+
+-- Trigger: trigger_or on "OLYMPIC_RANKING"
+
+-- DROP TRIGGER trigger_or ON "OLYMPIC_RANKING";
+
+CREATE TRIGGER trigger_or
+  AFTER INSERT OR UPDATE OR DELETE
+  ON "OLYMPIC_RANKING"
+  FOR EACH ROW
+  EXECUTE PROCEDURE "UPDATE_REF"('OR');
