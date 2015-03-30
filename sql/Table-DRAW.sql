1cc0cd039b5d97fbb41d92efc191b269cc300610
Guillaume TRUC <guillaume.truc@gmail.com>
2015-03-30 20:38:41 +0200

    Create Table-DRAW.sql

---
+++ sql/Table-DRAW.sql
@@ -0,0 +1,37 @@
+-- Table: "DRAW"
+
+-- DROP TABLE "DRAW";
+
+CREATE TABLE "DRAW"
+(
+  id integer NOT NULL,
+  id_result integer NOT NULL,
+  id1_qf1 integer,
+  id2_qf1 integer,
+  id1_qf2 integer,
+  id2_qf2 integer,
+  id1_qf3 integer,
+  id2_qf3 integer,
+  id1_qf4 integer,
+  id2_qf4 integer,
+  id1_sf1 integer,
+  id2_sf1 integer,
+  id1_sf2 integer,
+  id2_sf2 integer,
+  result_qf1 character varying(40),
+  result_qf2 character varying(40),
+  result_qf3 character varying(40),
+  result_qf4 character varying(40),
+  result_sf1 character varying(40),
+  result_sf2 character varying(40),
+  id_member integer NOT NULL,
+  last_update timestamp without time zone NOT NULL DEFAULT now(),
+  first_update timestamp without time zone NOT NULL DEFAULT now(),
+  id1_thd integer,
+  id2_thd integer,
+  result_thd character varying(40),
+  CONSTRAINT "DRAW_pkey" PRIMARY KEY (id)
+)
+WITH (
+  OIDS=FALSE
+);
