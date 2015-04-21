dae08304c07902d53b87e87bef39130204937cc8
Guillaume TRUC <guillaume.truc@gmail.com>
2015-04-21 22:15:45 +0200

    First Android commit

---
+++ android/app/src/main/java/com/sporthenon/data/IResultItem.java
@@ -0,0 +1,9 @@
+package com.sporthenon.data;
+
+public interface IResultItem {
+
+    public int getId();
+
+    public void setId(int id);
+
+}
---
+++ android/app/src/main/java/com/sporthenon/utils/AndroidUtils.java
@@ -0,0 +1,49 @@
+package com.sporthenon.utils;
+
+import android.app.Activity;
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.drawable.BitmapDrawable;
+import android.util.Log;
+
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+public class AndroidUtils {
+
+    public static BitmapDrawable getImage(Activity activity, String alias, String img, Integer id) {
+        BitmapDrawable bd = null;
+        HttpURLConnection connection = null;
+        try {
+            File dir = activity.getApplicationContext().getDir("sh", Context.MODE_PRIVATE);
+            File file = new File(dir, alias + id + ".png");
+            if (file.exists())
+                bd = new BitmapDrawable(new FileInputStream(file));
+            else {
+                connection = (HttpURLConnection) new URL(img) .openConnection();
+                connection.connect();
+                InputStream input = connection.getInputStream();
+                bd = new BitmapDrawable(BitmapFactory.decodeStream(input));
+                connection.disconnect();
+
+                ByteArrayOutputStream stream = new ByteArrayOutputStream();
+                bd.getBitmap().compress(Bitmap.CompressFormat.PNG, 100, stream);
+                byte[] arr = stream.toByteArray();
+                FileOutputStream output = new FileOutputStream(file);
+                output.write(arr);
+                output.close();
+            }
+        }
+        catch (Exception e) {
+            Log.e("Error", e.getMessage(), e);
+        }
+        return bd;
+    }
+
+}
